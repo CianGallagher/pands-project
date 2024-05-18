a4e1fe58bd530a4fb72a0e20c21107df0380c653
Gally <ciangall1@gmail.com>
2024-05-18 16:37:54 +0100

    libraries and dataset imported and columns defined

--- analysis.py
+++ analysis.py
@@ -1,1 +1,13 @@
+# Pandas and pyplot import - these libraries are used to plot and analyse the data.
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Loading the Iris dataset directly from the source github repository and assigning to variable df, s
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/iris-data.csv')
+
+# Columns defined
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+
+# Import to be tested once library install issues resolved
+
 
