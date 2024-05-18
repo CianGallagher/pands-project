660dc5333a263e906feab5d6e23e22f3de030d0b
Gally <ciangall1@gmail.com>
2024-05-18 17:24:56 +0100

    import issues resolved & summary.txt output test successful

--- analysis.py
+++ analysis.py
@@ -2,12 +2,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Loading the Iris dataset directly from the source github repository and assigning to variable df, s
+# Loading the Iris dataset directly from the source github repository and assigning to variable df
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/iris-data.csv')
 
 # Columns defined
 column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
-# Import to be tested once library install issues resolved
+# Import to be tested once library install issues resolved on linux laptop
+with open('summary.txt', 'w') as file:
+    # Statistics summary
+    file.write("Statistics Summary:\n")
+    file.write(df.describe().to_string())
 
 
