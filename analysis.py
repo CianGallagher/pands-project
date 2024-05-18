c56ce0ed6405ea0adebe73571082640b030d834d
Gally <ciangall1@gmail.com>
2024-05-18 17:37:33 +0100

    added a species count to the summery.txt output, needs reducing as currently unreadable

--- analysis.py
+++ analysis.py
@@ -8,10 +8,14 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/iris-
 # Columns defined
 column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
-# Import to be tested once library install issues resolved on linux laptop
+# Testing summary output
 with open('summary.txt', 'w') as file:
     # Statistics summary
-    file.write("Statistics Summary:\n")
+    file.write("Statistics Summary:\n") 
+    # Writing the descibe method to a string on the dataframe (df)
     file.write(df.describe().to_string())
+    # Need to reduce the value count per species down to just a class count.... output is unreadable currently
+    file.write('\n\nSpecies:\n')
+    file.write(df.value_counts().to_string())
 
 
